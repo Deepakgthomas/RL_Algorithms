b4113b615d65008a439c95df3eb7474fcef7b2a1
dgthomas <dgthomas@iastate.edu>
2023-02-23 20:30:11 -0600

    progress with sac

--- sac.py
+++ sac.py
@@ -9,6 +9,7 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from collections import deque
+from torch.distributions.normal import Normal
 import torchvision as tv
 import torch.nn.functional as F
 import torch.optim as optim
@@ -17,8 +18,11 @@ value_lr = 0.001
 policy_lr = 0.001
 batch_size = 100
 episodes = 1000
-env = gym.make("Pendulum-v1")
+ent_coeff = 0.001 #todo What's the value for this?
+gamma = 0.99
+Q_learning_rate = 0.001
 
+env = gym.make("MountainCarContinuous-v0")
 class Q_function(nn.Module):
     def __init__(self, state_size, action_size):
         super(Q_function, self).__init__()
@@ -46,7 +50,7 @@ class PolicyNetwork(nn.Module):
         self.linear3.weight.data.uniform_(-init_w, init_w)
         self.linear3.bias.data.uniform_(-init_w, init_w)
 
-    def forward(self, state):
+    def forward(self, state, deterministic = False):
         mu = F.relu(self.linear1(state))
         mu = F.relu(self.linear2(mu))
         mu = F.tanh(self.linear3(mu))
@@ -54,19 +58,36 @@ class PolicyNetwork(nn.Module):
         sigma = F.relu(self.linear1(state))
         sigma = F.relu(self.linear2(sigma))
         sigma = F.tanh(self.linear3(sigma))
-        epsilon = torch.normal(0,1)
-        action = F.tanh(mu + sigma*epsilon)
 
-        return action
+        dist = Normal(mu, sigma)
+        if not deterministic:
+            action = dist.rsample()
+        else:
+            action = mu
+
+        #Copying this from here - https://github.com/openai/spinningup/blob/master/spinup/algos/pytorch/sac/core.py
+        #Need to understand it better
+        log_pi = dist.log_prob(action).sum(axis=-1)
+        log_pi -= (2*(np.log(2)-action-F.softplus(-2*action))).sum(axis=1)
+
+        #todo Confused with the location of log prob
+
+        action = F.tanh(action)
+
+
+
+
+        return action, log_pi
 replay_buffer = deque(maxlen=10000)
 
 Q1 = Q_function(env.observation_space.shape[-1], env.action_space.n)
+
 # Q2 = Q_function(env.observation_space.shape[-1], env.action_space.n)
 
 policy = PolicyNetwork(env.observation_space.shape[-1], env.action_space.n)
+Q1_opt = torch.optim.Adam(params = Q1.parameters(), lr = Q_learning_rate)
+policy_opt = torch.optim.Adam(params = policy.parameters(), lr = policy_lr)
 
-value_opt = optim.Adam(Q1.parameters(), lr=value_lr)
-policy_opt = optim.Adam(policy.parameters(), lr = policy_lr)
 
 def update():
     state, next_state, reward, done, action = zip(*random.sample(replay_buffer, batch_size))
@@ -75,15 +96,21 @@ def update():
     next_state = torch.from_numpy(np.array(next_state)).reshape(batch_size, 3, 210, 160).type(torch.float32)
     reward = torch.from_numpy(np.array(reward))
     done = torch.from_numpy(np.array(done)).long()
-    next_action = policy(next_state).cpu().detach().numpy()
-    new_action = policy(state).cpu().detach().numpy()
+    curr_policy_next_action = policy(next_state)[0].cpu().detach().numpy()
+    curr_policy_action = policy(state)[0].cpu().detach().numpy()
     current_Q = Q1(state, action)
-    current_Q_new = Q1(state, new_action)
-    next_state_Q = Q1(next_state, new_action)
-    target = reward + gamma*(1-done)(next_state_Q - lr*log(next_action))
-    Q_loss = (current_Q - target)**2
-    policy_loss = (current_Q_new)
+    current_Q_new = Q1(state, curr_policy_action)
+    next_state_Q = Q1(next_state, curr_policy_next_action)
+    target = reward + gamma*(1-done)(next_state_Q - ent_coeff*policy(next_state)[1])
+    Q_loss = ((current_Q - target)**2).mean()
+    Q1_opt.zero_grad()
+    Q_loss.backward()
+    Q1_opt.step()
 
+    policy_loss = (current_Q_new-ent_coeff*torch.log(policy(next_state)[1])).mean()
+    policy_opt.zero_grad()
+    policy_loss.backward()
+    policy_opt.step()
 
 
 for i in range(episodes):
