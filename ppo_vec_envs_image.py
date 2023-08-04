e202f572e02602c7a1890ce331babebf01cc0bf4
dgthomas <dgthomas@iastate.edu>
2023-08-04 12:00:31 -0500

    Got vectorized PPO working for images!
    Non image PPO works quite well now

--- ppo_vec_envs_image.py
+++ ppo_vec_envs_image.py
@@ -4,32 +4,41 @@
 if __name__ == '__main__':
 
     import numpy as np
-    import gym
+    import gymnasium as gym
+    from gymnasium.wrappers import AtariPreprocessing, FrameStack, GrayScaleObservation, TransformObservation
+
     import torch
     import random
     import matplotlib.pyplot as plt
     from torch import nn
-
+    import torchvision as tv
+    import torch.nn.functional as F
     torch.manual_seed(798)
     import matplotlib.pyplot as plt
     torch.manual_seed(0)
     random.seed(0)
     np.random.seed(0)
     from collections import deque
-    num_envs = 20
-    batches = 4
-    env = gym.vector.make('Acrobot-v1', num_envs=num_envs)
-    env.seed(0)
+    num_envs = 12
+    num_stack = 3
+    batches = 30
+    channels = 3
+    def reshape_image(obs):
+        new_obs = np.array(obs).reshape(num_envs, 210, 160, 3*num_stack)
+        return new_obs
+    env = gym.vector.make("Pong-v4", num_envs=num_envs)
+    env = FrameStack(env, num_stack=num_stack)
+    env = TransformObservation(env, reshape_image)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     learning_rate = 0.00025
-    episodes = 200
+    episodes = 500
     gamma = 0.99
     clip = 0.2
 
     #No idea whether these hyperparameters are good
-    ppo_batch = 100
-    training_iters = 40
+    rollout_steps = 50
+    training_iters = 15
 
 
     # dim_action = env.action_space.shape[0]
@@ -39,15 +48,28 @@ if __name__ == '__main__':
             super(Actor, self).__init__()
             self.state_size = state_size
             self.action_size = action_size
-            self.linear_relu_stack = nn.Sequential(
-                nn.Linear(state_size, 128),
-                nn.ReLU(),
-                nn.Linear(128, 128),
-                nn.ReLU(),
-                nn.Linear(128, action_size),
-                nn.Softmax(dim=-1))
+            self.grayscale = tv.transforms.Grayscale()
+            self.conv1 = nn.Conv2d(num_stack, 6, 5)
+            self.pool = nn.MaxPool2d(2, 2)
+            self.conv2 = nn.Conv2d(6, 16, 3)
+            self.conv3 = nn.Conv2d(16, 32, 3)
+            self.fc1 = nn.Linear(13824, 120)
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, action_size)
+            self.last = nn.Softmax(dim=-1)
         def forward(self,x):
-            x = self.linear_relu_stack(x)
+            x = x.reshape(-1, 210, 160, num_stack, 3)
+            x = x.permute(0, 3, 4, 2, 1)
+            x = self.grayscale(x)
+            x = x.reshape(-1, num_stack, 210, 160)
+            x = self.pool(F.relu(self.conv1(x)))
+            x = self.pool(F.relu(self.conv2(x)))
+            x = self.pool(F.relu(self.conv3(x)))
+            x = torch.flatten(x, 1)
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = self.fc3(x)
+            x = self.last(x)
             return x
 
     class Critic(nn.Module):
@@ -55,25 +77,39 @@ if __name__ == '__main__':
             super(Critic, self).__init__()
             self.state_size = state_size
             self.action_size = action_size
-            self.linear_stack = nn.Sequential(
-                nn.Linear(state_size, 300),
-                nn.ReLU(),
-                nn.Linear(300, 128),
-                nn.ReLU(),
-                nn.Linear(128, 128),
-                nn.ReLU(),
-                nn.Linear(128, 1)
-            )
+            self.grayscale = tv.transforms.Grayscale()
+            self.conv1 = nn.Conv2d(num_stack, 6, 5)
+            self.pool = nn.MaxPool2d(2, 2)
+            self.conv2 = nn.Conv2d(6, 16, 3)
+            self.conv3 = nn.Conv2d(16, 32, 3)
+            self.fc1 = nn.Linear(13824, 120)
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, 1)
 
         def forward(self, x):
-            x = self.linear_stack(x)
+            x = x.reshape(-1, 210, 160, num_stack, 3)
+            x = x.permute(0, 3, 4, 2, 1)
+
+            x = self.grayscale(x)
+
+            x = x.reshape(-1, num_stack, 210, 160)
+
+            x = self.pool(F.relu(self.conv1(x)))
+            x = self.pool(F.relu(self.conv2(x)))
+            x = self.pool(F.relu(self.conv3(x)))
+            x = torch.flatten(x, 1)
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = self.fc3(x)
             return x
 
-    actor = Actor(env.observation_space.shape[1], env.action_space[0].n).to(device)
-    critic = Critic(env.observation_space.shape[1], 1).to(device)
+    actor = Actor(env.observation_space.shape[-1], env.action_space[0].n).to(device)
+    critic = Critic(env.observation_space.shape[-1], 1).to(device)
     policy_opt = torch.optim.Adam(params = actor.parameters(), lr = learning_rate)
     value_opt = torch.optim.Adam(params = critic.parameters(), lr = learning_rate)
-    obs = torch.tensor(env.reset(), dtype=torch.float32).to(device)
+    obs = torch.tensor(env.reset()[0], dtype=torch.float32).to(device) #Gymnasium is quite different from gym
+
+
     tot_rewards = np.array([0 for i in range(num_envs)], dtype=float)
     final_scores = []
     last_n_rewards = deque(maxlen=10)
@@ -86,17 +122,18 @@ if __name__ == '__main__':
         all_observations = []
         all_dones = []
         global tot_rewards #Why did I have to declare tot_rewards as global?
-        for i in range(ppo_batch):
+        for i in range(rollout_steps):
+
+            foo = actor(obs.to(device))
             act_probs = torch.distributions.Categorical(actor(obs.to(device)).squeeze())
             action = act_probs.sample().squeeze()
             action = action.cpu().detach().numpy()
 
-            next_state, reward, done, info = env.step(action)
+            next_state, reward, done, truncated, info = env.step(action)
 
             action = torch.tensor(action, dtype=torch.float32).to(device)
             all_rewards.append(reward)
             tot_rewards += reward
-            # print("tot_rewards = ", tot_rewards)
             for reward_val, done_val in zip(tot_rewards, done):
                 if done_val:
                     print("reward_val = ", reward_val)
@@ -108,7 +145,7 @@ if __name__ == '__main__':
             all_actions.append(action.cpu().detach().numpy())
             all_actions_probs.append(act_probs.log_prob(action).cpu().detach().numpy())
 
-            obs = torch.tensor(next_state, dtype=torch.float32).unsqueeze(0)
+            obs = torch.tensor(next_state, dtype=torch.float32)
         eps_rew = critic(obs.to(device)).cpu().detach().numpy().reshape(num_envs)
         eps_rew_list = []
 
@@ -131,8 +168,12 @@ if __name__ == '__main__':
         return batch_obs, batch_act, batch_log_probs, batch_rtgs, obs
 
     for i in range(episodes):
-        print("i = ", i)
+        print("episodes = ", i)
         all_obs, all_act, all_log_probs, all_rtgs, obs = rollout(obs)
+        all_obs = all_obs.reshape(-1,210,160,num_stack, channels)
+
+
+
         value = critic(all_obs).squeeze()
 
         # todo Why are we detaching value
@@ -144,7 +185,7 @@ if __name__ == '__main__':
 
         for _ in range(training_iters):
 
-            total_examples = num_envs * ppo_batch
+            total_examples = num_envs * rollout_steps
             batch_size = total_examples // batches
             batch_starts = np.arange(0, total_examples, batch_size)
             indices = np.arange(total_examples, dtype=np.int32)
@@ -167,15 +208,14 @@ if __name__ == '__main__':
 
                 value = critic(batch_obs).squeeze()
                 assert(value.ndim==1)
-                policy = actor(batch_obs).squeeze()
+                policy = actor(batch_obs)
 
                 act_probs = torch.distributions.Categorical(policy)
+
                 log_probs = act_probs.log_prob(batch_act).squeeze()
-                # print("log_probs = ", log_probs)
 
                 ratios = torch.exp(log_probs - batch_log_probs)
                 assert(ratios.ndim==1)
-                # print("ratios = ", ratios.shape)
                 surr1 = ratios*batch_advantages
                 assert (surr1.ndim == 1)
                 surr2 = torch.clamp(ratios, 1 - clip, 1 + clip)*batch_advantages
